7906a785ab3d08a8e7c0e6e03078e8d9a28a20b4
ellenlee <leechinghui.tw@gmail.com>
2016-12-25 22:41:01 +0800

    解決WordPress style.css 修改後沒反應的問題

--- wp-content/themes/minzu/functions.php
+++ wp-content/themes/minzu/functions.php
@@ -361,4 +361,10 @@ function grasstw_youtube_vid( $atts ){
 
 add_shortcode('youtube', 'grasstw_youtube_vid');
 
+// 解決WordPress樣式修改後沒反應的問題
 
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+function enqueue_child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), NULL, filemtime( get_stylesheet_directory() . '/style.css' ) );
+}
